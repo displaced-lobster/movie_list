0e80f784cfdffea4fb53fa9f2895a45143fef054
scripts-richard <scripts.richard@gmail.com>
2016-12-02 21:46:07 -0700

    Working email helper and grabs emails from text file.

--- movies.py
+++ movies.py
@@ -10,9 +10,8 @@ import bs4 as bs
 import urllib.request
 import json
 import requests
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+
+import gmail_helper
 
 def isFloat(num):
     try:
@@ -23,9 +22,9 @@ def isFloat(num):
 
 def imdbData(movie, year):
     url = 'http://www.omdbapi.com/?t='
-    endUrl = '&y=' + year + '&plot=short&r=json'
-    urlSearch = url + movie.replace(" ", "+") + endUrl
-    resp = requests.get(url = urlSearch)
+    end_url = '&y=' + year + '&plot=short&r=json'
+    url_search = url + movie.replace(" ", "+") + end_url
+    resp = requests.get(url = url_search)
     data = json.loads(resp.text)
     if data['Response'] == 'True':
         rating = data['imdbRating']
@@ -35,71 +34,58 @@ def imdbData(movie, year):
                 return [movie, rating, link, data['Plot']]
     return -1
 
-def email(subject, text, html):
-    d = shelve.open('email_helper')
-    username = d['from_address']
-    password = d['password']
-    toAddresses = d['to_addresses']
+def main ():
+    source = urllib.request.urlopen('http://www.edmovieguide.com/movies/?sort=release-date')
+    soup = bs.BeautifulSoup(source, 'lxml')
+    movie_list = []
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = username
-    msg['To'] = ', '.join(toAddresses)
+    for li in soup.find_all('li', class_ = 'movie'):
+        try:
+            movie_name = li.find('a', class_ = 'movie-title').text
+            try:
+                movie_date = li.find('p', class_ = 'movie-date').text[-4:]
+            except:
+                movie_date = ''
+            movie_list.append([movie_name, movie_date])
+        except:
+            pass
 
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+    good_movies = []
 
-    msg.attach(part1)
-    msg.attach(part2)
+    for movie in movie_list:
+        movie_data = imdbData(movie[0], movie[1])
+        if movie_data != -1:
+            good_movies.append(movie_data)
 
-    server = smtplib.SMTP('smtp.gmail.com:587') # Change if not using gmail
-    server.ehlo()
-    server.starttls()
-    server.login(username, password)
-    server.sendmail(username, toAddresses, msg.as_string())
-    server.quit()
-    return
+    movie_text = ''
+    movie_HTML = ''
 
-source = urllib.request.urlopen('http://www.edmovieguide.com/movies/?sort=release-date')
-soup = bs.BeautifulSoup(source, 'lxml')
-movie_list = []
+    for movie in good_movies:
+        movie_text += movie[0] + '     ' + movie[1] + '\n'
+        movie_text += movie[3] + '\n'
+        movie_HTML += '<tr><td><a href="%s">%s</a></td>' %(movie[2], movie[0])
+        movie_HTML += "<td>%s</td></tr>" %movie[1]
+        movie_HTML += "<tr><td>%s</td></tr>" %movie[3]
 
-for li in soup.find_all('li', class_ = 'movie'):
-    movie_name = li.find('a', class = 'movie-title').text
-    movie_date = li.find('p', class = 'movie-date').text[-4:]
-    movie_list.append([movie_name, movie_date])
+    text = "Top Rated Movies Playing in Edmonton!\n\n"
+    text += movie_text
+    html = """\
+    <html>
+        <head></head>
+        <body>
+            <h1>Top Rated Movies Playing in Edmonton!</h1>
+            <br>
+            <table>
+                <tr><th>Movie</th><th>Rating</th></tr>
+                %s
+            </table>
+        </body>
+    </html>
+    """ %movie_HTML
 
-goodMovies = []
+    with open('emails.txt') as f:
+        emails = f.read().splitlines()
 
-for movie in movie_list:
-    movieData = imdbData(movie[0], movie[1])
-    if movieData != -1:
-        goodMovies.append(movieData)
+    sender = emails.pop(0)
 
-movieText = ''
-movieHTML = ''
-
-for movie in goodMovies:
-    movieText += movie[0] + '     ' + movie[1] + '\n'
-    movieText += movie[3] + '\n'
-    movieHTML += '<tr><td><a href="%s">%s</a></td>' %(movie[2], movie[0])
-    movieHTML += "<td>%s</td></tr>" %movie[1]
-    movieHTML += "<tr><td>%s</td></tr>" %movie[3]
-
-text = "Top Rated Movies Playing in Edmonton!\n\n"
-text += movietext
-html = """\
-<html>
-    <head></head>
-    <body>
-        <h1>Top Rated Movies Playing in Edmonton!</h1>
-        <br>
-        <table>
-            <tr><th>Movie</th><th>Rating</th></tr>
-            %s
-        </table>
-    </body>
-</html>
-""" %movieHTML
-
-email(toAddresses, "Movie List", text, html)
+    gmail_helper.email(sender, emails, 'Movie List', text, html)
